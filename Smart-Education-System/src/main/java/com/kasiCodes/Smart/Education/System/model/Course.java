74296cf9f75ada17850450ded66db8eaf838d1bf
mrampai023 <mrampai023@student.wethinkcode.co.za>
2024-09-17 14:01:40 +0200

    updated course class

--- Smart-Education-System/src/main/java/com/kasiCodes/Smart/Education/System/model/Course.java
+++ Smart-Education-System/src/main/java/com/kasiCodes/Smart/Education/System/model/Course.java
@@ -1,5 +1,81 @@
 package com.kasiCodes.Smart.Education.System.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "courses")
+
 public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "course_name", nullable = false)
+    private String courseName;
+
+    @Column(name = "course_description", nullable = false)
+    private String courseDescription;
+
+    @Column(name = "course_duration", nullable = false)
+    private int courseDuration;
+
+    @Column(name = "course_teacher", nullable = false)
+    private String courseTeacher;
+
+
+    public Course() {
+    }
+
+    public Course(String courseName, String courseDescription, int courseDuration, String courseTeacher) {
+        this.courseName = courseName;
+        this.courseDescription = courseDescription;
+        this.courseDuration = courseDuration;
+        this.courseTeacher = courseTeacher;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseDescription() {
+        return courseDescription;
+    }
+
+    public void setCourseDescription(String courseDescription) {
+        this.courseDescription = courseDescription;
+    }
+
+    public int getCourseDuration() {
+        return courseDuration;
+    }
+
+    public void setCourseDuration(int courseDuration) {
+        this.courseDuration = courseDuration;
+    }
+
+    public String getCourseTeacher() {
+        return courseTeacher;
+    }
+
+    public void setCourseTeacher(String courseTeacher) {
+        this.courseTeacher = courseTeacher;
+    }
     
 }
