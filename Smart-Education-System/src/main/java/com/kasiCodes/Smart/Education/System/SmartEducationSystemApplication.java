ff2d3d01d15aeec0cba0af9299bde393cb13892c
mrampai023 <mrampai023@student.wethinkcode.co.za>
2024-09-17 12:34:37 +0200

    init commit

---
+++ Smart-Education-System/src/main/java/com/kasiCodes/Smart/Education/System/SmartEducationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.kasiCodes.Smart.Education.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartEducationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartEducationSystemApplication.class, args);
+	}
+
+}
---
+++ Smart-Education-System/src/test/java/com/kasiCodes/Smart/Education/System/SmartEducationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kasiCodes.Smart.Education.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEducationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
