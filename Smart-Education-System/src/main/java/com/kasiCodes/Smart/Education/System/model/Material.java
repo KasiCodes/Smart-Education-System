da3615b8bb051b4e235245be03a94789812d51a8
mrampai023 <mrampai023@student.wethinkcode.co.za>
2024-09-18 13:24:48 +0200

    updated quiz class

--- Smart-Education-System/src/main/java/com/kasiCodes/Smart/Education/System/model/Material.java
+++ Smart-Education-System/src/main/java/com/kasiCodes/Smart/Education/System/model/Material.java
@@ -1,16 +1,66 @@
 package com.kasiCodes.Smart.Education.System.model;
 
+import org.hibernate.annotations.ManyToAny;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "material")
 public class Material {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "material_name", nullable = false)
     private String materialName;
+
+    @Column(name = "material_url", nullable = false)
     private String materialUrl; 
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "course_id", nullable = false)
+    private Course course;
 
     public Material() {
     }
     public Material(String materialName, String materialUrl) {
         this.materialName = materialName;
         this.materialUrl = materialUrl;
+    }
+
+    public Material(String materialName, String materialUrl, Course course) {
+        this.materialName = materialName;
+        this.materialUrl = materialUrl;
+        this.course = course;
+    }
+
+    public Material(Long id, String materialName, String materialUrl, Course course) {
+        this.id = id;
+        this.materialName = materialName;
+        this.materialUrl = materialUrl;
+        this.course = course;
+    }
+
+    public Material(Long id, String materialName, String materialUrl) {
+        this.id = id;
+        this.materialName = materialName;
+        this.materialUrl = materialUrl;
+    }
+    
+
+    public Course getCourse() {
+        return course;
+    }
+    public void setCourse(Course course) {
+        this.course = course;
     }
 
     public Long getId() {
--- Smart-Education-System/src/main/java/com/kasiCodes/Smart/Education/System/model/Quiz.java
+++ Smart-Education-System/src/main/java/com/kasiCodes/Smart/Education/System/model/Quiz.java
@@ -1,5 +1,87 @@
 package com.kasiCodes.Smart.Education.System.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "quiz")
 public class Quiz {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "quiz_name", nullable = false)
+    private String quizName;
+
+    @Column(name = "quiz_description", nullable = false)
+    private String quizDescription;
+
+    @Column(name = "quiz_duration", nullable = false)
+    private int quizDuration;
+
+    @Column(name = "quiz_passing_percentage", nullable = false)
+    private int quizPassingPercentage;
+
+    @Column(name = "quiz_status", nullable = false)
+    private boolean quizStatus = false;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "course_id", nullable = false)
+    private Course course;
+
+
+    public Quiz() {
+    }
+
+    public Quiz(String quizName, String quizDescription, int quizDuration, int quizPassingPercentage) { 
+        this.quizName = quizName;
+        this.quizDescription = quizDescription;
+        this.quizDuration = quizDuration;
+        this.quizPassingPercentage = quizPassingPercentage;
+    }
+
+    public Quiz(Long id, String quizName, String quizDescription, int quizDuration, int quizPassingPercentage) {
+        this.id = id;
+        this.quizName = quizName;
+        this.quizDescription = quizDescription;
+        this.quizDuration = quizDuration;
+        this.quizPassingPercentage = quizPassingPercentage;
+    }
+
+    public boolean isQuizStatus() {
+        return quizStatus;
+    }
+
+    public void setQuizStatus(boolean quizStatus) {
+        this.quizStatus = quizStatus;
+    }   
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getQuizName() {
+        return quizName;
+    }
+
+    public void setQuizName(String quizName) {
+        this.quizName = quizName;
+    }
+
+    public String getQuizDescription() {
+        return quizDescription;
+    }
+
     
 }
